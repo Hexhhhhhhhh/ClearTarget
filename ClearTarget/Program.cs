3feb8251c0dc072d72e42aded8af82a34bc89d39
Hexhhhhhhhh <3268983565@qq.com>
2024-07-24 17:15:11 +0800

    Change Version

--- ClearTarget/Program.cs
+++ ClearTarget/Program.cs
@@ -4,7 +4,7 @@ namespace ClearTarget
 {
     class Program
     {
-        public static string Version = "V1.0.7.20240723_beta";
+        public static string Version = "V1.0.1.20240724_release";
         static async Task Main(string[] args)
         {
             await MainEntrance.MainShow();
